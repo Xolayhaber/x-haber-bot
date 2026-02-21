58652e25120e9489cce5712e1a185c63c2ef9151
Xolayhaber <leqendary@hotmail.com>
2026-02-21 13:25:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-print("Xolayhaber çalışıyor")
-
-haber = "Yapay zeka sektöründe büyük yatırım dalgası başladı."
-
-if len(haber) <= 280:
-    print("Tweet hazır:")
-    print(haber)
-else:
-    print("Metin çok uzun")
+print("TEST BASLADI")
+exit()
