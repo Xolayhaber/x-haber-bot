ae3b2069a288c688732829a53728ad458487eaf7
Xolayhaber <leqendary@hotmail.com>
2026-02-21 12:53:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-print("TEST BAŞLADI")
-
-import feedparser
-
-rss_url = "https://feeds.bbci.co.uk/news/technology/rss.xml"
-feed = feedparser.parse(rss_url)
-
-print("Toplam haber sayısı:", len(feed.entries))
-
-for entry in feed.entries[:3]:
-    print("Haber:", entry.title)
-
-print("TEST BİTTİ")
+print("SADECE BU YAZI GÖRÜNMELİ")
