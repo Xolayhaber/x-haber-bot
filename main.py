0d631686ba912de52990a9532c0179eed581d210
Xolayhaber <leqendary@hotmail.com>
2026-03-14 21:44:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("BOT BASARIYLA CALISTI")
+from news_fetcher import get_news
+
+print("BOT BASLADI")
+
+news = get_news()
+
+for n in news[:5]:
+    print("HABER:")
+    print(n["title"])
+    print(n["link"])
+    print("------")
