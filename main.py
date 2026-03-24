51f3d8734ff8b28aae3f9fc66e4494072ad33dbf
Xolayhaber <leqendary@hotmail.com>
2026-03-24 21:12:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from news_fetcher import get_news
 print("BOT BASLADI")
 
 def ozetle(metin):
-    # basit özet (ilk 12 kelime)
     kelimeler = metin.split()
-    return " ".join(kelimeler[:12]) + "..."
+
+    if len(kelimeler) > 12:
+        return " ".join(kelimeler[:12]) + "..."
+    else:
+        return metin
 
 news = get_news()
 
 for n in news[:5]:
-    ozet = ozetle(n["title"])
+    ozet = ozetle(n.get("summary", ""))
 
     tweet = f"""📰 SON DAKİKA
 
