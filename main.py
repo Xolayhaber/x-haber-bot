f7751186b8a3d739cf491c674b4879045548f871
Xolayhaber <leqendary@hotmail.com>
2026-03-24 21:19:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from news_fetcher import get_news
 
 print("BOT BASLADI")
 
+# yasaklı kelimeleri oku
+with open("banned_words.txt", "r") as f:
+    banned_words = [line.strip().lower() for line in f.readlines()]
+
+def temiz_mi(metin):
+    for kelime in banned_words:
+        if kelime in metin.lower():
+            return False
+    return True
+
 news = get_news()
 
 for n in news[:5]:
+
+    if not temiz_mi(n["title"]):
+        print("❌ Filtreye takıldı:", n["title"])
+        continue
 
     tweet = f"""📰 SON DAKİKA
 
